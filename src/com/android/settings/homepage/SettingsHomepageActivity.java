8138230dbf461b42218d1dab8b03399a9a11f971
G-H-O-5-T <jijinraj401@gmail.com>
2021-02-05 01:24:18 +0530

    Implement New Day/Night Greeting Modes With Animations in Settings HomePage UI.
    - Team Spice: Initial Implementation.
    - Modit: Create a Switch, New HomePage Background and Layout Fixes.
    - ElectroPerf: Code Improvements and Patches.
    - Kqixs Lofui: Introduced Immersive Homepage, Fix Some Timings, brought up "Alien UFO Time Greetings", Fix Homepage TextView and Updated CardView Colors.
    - Punvy: Special Thanks......Added Liquid background and code improvements.
    - Also Thanks to @mickenkng for translations.
    - Rahul: New HomepPage Backgrounds.
    
    Co-Authored-by: kqixs <kqixs@users.noreply.github.com>
    Co-Authored-by: modit jain <moditjain121@gmail.com>
    Co-Authored-by: ElectroPerf <kunmun.devroms@gmail.com>
    Co-Authored-by: G-H-O-5-T <jijinraj401@gmail.com>
    Co-Authored-by: mickenkng <mickenkng@yahoo.com>
    Co-Authored-by: Punvy <Punvy@users.noreply.github.com>
    Co-Authored-by: rahultammu <rahultammu@users.noreply.github.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.homepage;
 
+import android.content.res.Resources;
 import android.animation.LayoutTransition;
 import android.app.ActivityManager;
 import android.app.settings.SettingsEnums;
@@ -30,16 +31,28 @@ import android.os.UserManager;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.android.internal.util.UserIcons;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceFragmentCompat;
 
+import com.android.internal.util.UserIcons;
+import com.android.settings.DisplaySettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.accounts.AvatarViewMixin;
 import com.android.settings.core.HideNonSystemOverlayMixin;
@@ -47,6 +60,8 @@ import com.android.settings.homepage.contextualcards.ContextualCardsFragment;
 import com.android.settings.overlay.FeatureFactory;
 
 import com.android.settingslib.drawable.CircleFramedDrawable;
+import java.util.Calendar;
+import java.util.Random;
 
 public class SettingsHomepageActivity extends FragmentActivity {
 
@@ -68,6 +83,9 @@ public class SettingsHomepageActivity extends FragmentActivity {
         Context context = getApplicationContext();
 
         mUserManager = context.getSystemService(UserManager.class);
+
+        getRandomName();
+        goodVibesPlease();
 
         final Toolbar toolbar = findViewById(R.id.search_action_bar);
         FeatureFactory.getFactory(this).getSearchFeatureProvider()
@@ -110,6 +128,99 @@ public class SettingsHomepageActivity extends FragmentActivity {
         fragmentTransaction.commit();
     }
 
+    private void getRandomName(){
+    Resources res = getResources();
+    String[] array = res.getStringArray(R.array.random_user_names);
+    String randomName = array[new Random().nextInt(array.length)];
+    TextView homepageUsernameTextView=(TextView) findViewById(R.id.userNameTextView);
+    homepageUsernameTextView.setText(randomName);
+
+    }
+
+    private void goodVibesPlease(){
+
+    int NewDashAnim = Settings.System.getInt(getApplicationContext().getContentResolver(),
+        Settings.System.GREETING_MODE_ANIMATIONS, 1);
+        Calendar c = Calendar.getInstance();
+    int hours = c.get(Calendar.HOUR_OF_DAY);
+    String greeting = null;
+    TextView homePageGreetingTextView=(TextView) findViewById(R.id.greetingsTextView);
+    View root = findViewById(R.id.settings_homepage_container);
+
+    switch (NewDashAnim) {
+
+    case 0:
+
+    greeting = getGreetingMessage(hours);
+
+    break;
+
+    case 1:
+    setFullscreen();
+    setTextColorWhite();
+    greeting = getGreetingMessage(hours);
+    if(hours>=04 && hours<=11){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_morning));
+        setTextColorBlack();
+    } else if(hours>=12 && hours<=15){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_noon));
+        setTextColorBlack();
+    } else if(hours>=16 && hours<=20){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_evening));
+        setTextColorWhite();
+    } else if((hours>=21 && hours<=24) || hours==0 ){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_night));
+        setTextColorWhite();
+    } else {
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_ufo));
+        setTextColorWhite();
+    }
+
+    break;
+
+    case 2:
+    setFullscreen();
+    greeting = getGreetingMessage(hours);
+    if(hours>=04 && hours<=11){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_morning_liq));
+        setTextColorBlack();
+
+    } else if(hours>=12 && hours<=15){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_noon_liq));
+        setTextColorBlack();
+
+    } else if(hours>=16 && hours<=20){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_evening_liq));
+        setTextColorWhite();
+
+    } else if((hours>=21 && hours<=24) || hours==0 ){
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_night_liq));
+        setTextColorWhite();
+
+    } else {
+
+        root.setBackground(getResources().getDrawable(R.drawable.background_ufo_liq));
+        setTextColorWhite();
+
+    }
+
+    break;
+
+    }
+
+    homePageGreetingTextView.setText(greeting);
+
+ }
+
     @VisibleForTesting
     void setHomepageContainerPaddingTop() {
         final View view = this.findViewById(R.id.homepage_container);
@@ -138,6 +249,57 @@ public class SettingsHomepageActivity extends FragmentActivity {
     public void onResume() {
         super.onResume();
         avatarView.setImageDrawable(getCircularUserIcon(getApplicationContext()));
+        goodVibesPlease();
 
+    }
+    
+    private String getGreetingMessage(int hours){
+        Resources res = getResources();
+        String greeting_gm = res.getString(R.string.greeting_gm);
+        String greeting_ga = res.getString(R.string.greeting_ga);
+        String greeting_ge = res.getString(R.string.greeting_ge);
+        String greeting_gn = res.getString(R.string.greeting_gn);
+        String greeting_wte = res.getString(R.string.greeting_wte);
+        if(hours>=04 && hours<=11){
+
+            return greeting_gm;
+
+        } else if(hours>=12 && hours<=15){
+
+            return greeting_ga;
+
+        } else if(hours>=16 && hours<=20){
+
+            return greeting_ge;
+
+        } else if((hours>=21 && hours<=24) || hours==0 ){
+
+            return greeting_gn;
+
+        } else {
+
+            return greeting_wte;
+
+        }
+    }
+    private void setFullscreen(){
+        View root = findViewById(R.id.settings_homepage_container);
+        root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN );
+    }
+    private void setTextColorBlack(){
+        TextView homePageGreetingTextView=(TextView) findViewById(R.id.greetingsTextView);
+        TextView homepageUsernameTextView=(TextView) findViewById(R.id.userNameTextView);
+        TextView homepageSettingsTextView=(TextView) findViewById(R.id.settings_header);
+        homepageSettingsTextView.setTextColor(0xff000000);
+        homePageGreetingTextView.setTextColor(0xff000000);
+        homepageUsernameTextView.setTextColor(0xff000000);
+    }
+    private void setTextColorWhite(){
+        TextView homePageGreetingTextView=(TextView) findViewById(R.id.greetingsTextView);
+        TextView homepageUsernameTextView=(TextView) findViewById(R.id.userNameTextView);
+        TextView homepageSettingsTextView=(TextView) findViewById(R.id.settings_header);
+        homepageSettingsTextView.setTextColor(0xffffffff);
+        homePageGreetingTextView.setTextColor(0xffffffff);
+        homepageUsernameTextView.setTextColor(0xffffffff);
     }
 }
